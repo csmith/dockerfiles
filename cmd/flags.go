40443193a362da02680b3231beeb8c147dd6b340
Chris Smith <chris@chameth.com>
2021-10-23 00:53:21 +0100

    Switch to external tool for building

--- cmd/flags.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "flag"
-
-var (
-	registry     = flag.String("registry", "reg.c5h.io", "Registry to use for pushes and pulls")
-	registryUser = flag.String("registry-user", "", "Username to use when talking to docker registries")
-	registryPass = flag.String("registry-pass", "", "Password to use when talking to docker registries")
-)
